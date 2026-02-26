bae2851a7cb9987e1997c3d395839502bae6c3f9
Jamal Sambo <jhsambo99@gmail.com>
2026-02-26 18:15:06 +0200

    resolved bags 105

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -40,7 +40,7 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/forgot-password',
-    component: () => import('src/pages/auth/pages/ForgotPasswordPage.vue'),
+    component: () => import('@/pages/auth/pages/ForgotPasswordPage.vue'),
     meta: { guestOnly: true },
   },
   // Always leave this as last one,
